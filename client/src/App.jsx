f41cbfc519309b7422b20e81c84553476cdb0942
laxmanp090404 <laxman.p2022cse@sece.ac.in>
2024-10-18 22:19:53 +0530

    feat : TogglePlan component implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import TogglePlan from "./components/TogglePlan"
 function App() {
 
   return (
-   <section className="w-[100vw] h-[100vh] bg-white text-black">
+   <section className="w-[100vw] h-[100vh] bg-white flex flex-col justify-evenly items-center">
     <TogglePlan/>
     <Cards/>
    </section>
--- client/src/components/TogglePlan.jsx
+++ client/src/components/TogglePlan.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TogglePlan = () => {
+    const [isMonthly,setIsMonthly] = useState(true);
+    
   return (
-    <div>TogglePlan</div>
+    <div className='bg-[#E1EFFE] px-1 py-1 flex justify-between items-center rounded-lg font-semibold '>
+        <button 
+        className={`px-5 py-3 rounded-lg transition-colors duration-200 ${isMonthly ? 'bg-white' : ''}`} 
+        onClick={()=>{setIsMonthly(true)}}>Monthly</button>
+        <button 
+        className={`px-5 py-3 rounded-lg transition-colors duration-300 ${!isMonthly ? 'bg-white' : ''}`}  
+        onClick={()=>{setIsMonthly(false)}}>Annual</button>
+    </div>
   )
 }
 
