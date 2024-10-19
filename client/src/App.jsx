2d9cf82b1363d542153baf10973652b0956ee973
laxmanp090404 <laxman.p2022cse@sece.ac.in>
2024-10-19 22:56:17 +0530

    feat : Implemented the plan pricing cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import TogglePlan from "./components/TogglePlan"
 function App() {
 
   return (
-   <section className="w-[100vw] h-[100vh] bg-white flex flex-col justify-evenly items-center">
+   <section className="w-[100vw] min-h-[100vh] bg-white flex flex-col justify-between p-[6vh] gap-[4vh] items-center">
     <TogglePlan/>
     <Cards/>
    </section>
